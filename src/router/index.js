bc187af0488e0e78d1a23819d00e04a381ec8e43
nathawut-pook <rukawa_kkc@hotmail.com>
2020-06-20 13:25:26 +0700

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -62,19 +62,11 @@ const reptmdmWarehouseValue = () =>
 const mdmAR = () =>
     import ('@/views/report/mdmAR/mdmAR')
 
-<<<<<<< HEAD
-const Kexpenses = () =>
-    import ('@/views/report/Kexpenses')
-const Rroapercent = () =>
-    import ('@/views/report/roapercent')
-const RMaterial = () =>
-    import ('@/views/report/reptmaterial')
-=======
+
 const Kexpenses  = () =>import('@/views/report/Kexpenses')
 const Rroapercent = () =>import('@/views/report/roapercent')
 const RMaterial = () =>import('@/views/report/reptmaterial')
 const RBudgetProject = () =>import('@/views/report/reptbudgetproject')
->>>>>>> dbedd4cbdc0fb1c2bbbb41fec67f097a5f6b5752
 
 Vue.use(Router)
 
